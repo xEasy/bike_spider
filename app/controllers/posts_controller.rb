975fdf5288a2a5fb86bb244f387d5ebacb66e800
Yijie.Z <yijo1001@gmail.com>
2014-11-01 00:49:20 +0800

    打杂

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.where(title: /#{ params[:q] }/)
     @posts = @posts.where(from: params[:from]) if params[:from].present?
-    @posts = @posts.page(params[:page])
+    @posts = @posts.order(c_at: :desc).page(params[:page])
   end
 
   def show
--- app/workers/post_spider.rb
+++ app/workers/post_spider.rb
@@ -1,7 +1,19 @@
 require 'spider'
 
 class PostSpider
+  def fetch_all
+    @s_count = Post.count
+    @total_page = 1
+    perform('hz')
+    perform('qd')
+    perform('cb')
+
+    p "新获取: #{ Post.count - @s_count } 条交易记录"
+  end
+
   def perform(tag)
+    @s_count = Post.count
+    @total_page = 3
     case tag
     when 'hz'
       fetch_hz_bike
@@ -10,21 +22,22 @@ class PostSpider
     when 'qd'
       fetch_qd_bike
     end
+    p "新获取: #{ Post.count - @s_count } 条交易记录"
   end
 
   private
   def fetch_cb_bike
-    fetcher = Spider::Tags::CbBike.new(5)
+    fetcher = Spider::Tags::CbBike.new(@total_page)
     batch_create_post(fetcher.posts, fetcher)
   end
 
   def fetch_qd_bike
-    fetcher = Spider::Tags::QdBike.new(5)
+    fetcher = Spider::Tags::QdBike.new(@total_page)
     batch_create_post(fetcher.posts, fetcher)
   end
 
   def fetch_hz_bike
-    fetcher = Spider::Tags::HzBike.new(3)
+    fetcher = Spider::Tags::HzBike.new(@total_page)
     batch_create_post(fetcher.posts, fetcher)
   end
 
